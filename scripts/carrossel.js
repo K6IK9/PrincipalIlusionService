b71e4168aaae2036f9549bb7581294a2ca08069c
JoaoVictor-Noschang <joaovictornoschangsilva601@gmail.com>
2023-12-04 23:34:10 -0400

    Add conteudo
    Adicionando conteúdo a página.

--- scripts/carrossel.js
+++ scripts/carrossel.js
@@ -43,12 +43,12 @@ let galeria02 = document.querySelector('.sec_galeria02');
 let conteudo02 = document.querySelector('.conteudo02');
 let isMouseDown02 = false;
 let startX02;
-let scrollRight02;
+let scrollLeft02;
 
 galeria02.addEventListener('mousedown', (e) => {
   isMouseDown02 = true;
   startX02 = e.pageX - galeria02.offsetLeft;
-  scrollRight02 = galeria02.scrollWidth - galeria02.scrollLeft - galeria02.clientWidth;
+  scrollLeft02 = galeria02.scrollLeft;
 });
 
 galeria02.addEventListener('mouseleave', () => {
@@ -62,7 +62,7 @@ galeria02.addEventListener('mouseup', () => {
 galeria02.addEventListener('mousemove', (e) => {
   if (!isMouseDown02) return;
   e.preventDefault();
-  const x2 = e.pageX - galeria02.offsetLeft;
-  const walk2 = (x2 - startX02) * 1.1; // Mudar a direção do movimento
-  galeria02.scrollLeft = scrollRight02 + walk2; // Ajustar a posição da rolagem
+  const x02 = e.pageX - galeria02.offsetLeft;
+  const walk02 = (x02 - startX02) * 1.1;
+  galeria02.scrollLeft = scrollLeft02 - walk02;
 });
